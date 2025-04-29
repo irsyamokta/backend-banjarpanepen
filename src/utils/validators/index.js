06420bd0a6efd645d5b8c35901fbfeee05806a07
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 00:23:45 +0700

    Add : File endpoint

---
+++ src/utils/validators/index.js
@@ -0,0 +1,16 @@
+import { registerValidator, loginValidator } from "./validateAuth.js";
+import { updateUserValidator } from "./validateUser.js";
+// import { changePasswordValidator, resetPasswordValidator, forgotPasswordValidator } from "./validatePassword.js";
+// import { createNewsValidator, updateNewsValidator, newsStatusValidator } from "./validateNews.js";
+
+export { 
+    registerValidator, 
+    loginValidator, 
+    updateUserValidator,
+    // changePasswordValidator, 
+    // resetPasswordValidator, 
+    // forgotPasswordValidator,
+    // createNewsValidator,
+    // updateNewsValidator,
+    // newsStatusValidator
+};
