22c89543a01ba05fa6a2f20db9108a3594fd087e
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 00:20:15 +0700

    Add : User controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,28 @@
+import * as userService from "../services/user.service.js"
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const result = await userService.getUsers();
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const { result, message } = await userService.updateUser(req.user.id, req.body, req.file);
+        res.status(200).json({ message, data: result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteUser = async (req, res, next) => {
+    try {
+        const result = await userService.deleteUser(req.user.id);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
