7b62e7f14debd15e31a45ab1869d4a7667e8e453
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 00:20:00 +0700

    Add : Auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,60 @@
+import * as authService from "../services/auth.service.js";
+import * as tokenService from "../utils/token.utils.js";
+
+export const register = async (req, res, next) => {
+    try {
+        const result = await authService.register(req.body);
+        res.status(201).json({ status: "success", ...result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const login = async (req, res, next) => {
+    try {
+        const result = await authService.login(req.body, req, res);
+        res.status(200).json({ status: "success", ...result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const logout = async (req, res, next) => {
+    try {
+        await authService.logout(req.cookies);
+        res.clearCookie("refreshToken", tokenService.cookieOptions());
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const refreshToken = async (req, res, next) => {
+    try {
+        const result = await authService.refreshToken(req.cookies, req, res);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const me = async (req, res, next) => {
+    try {
+        const result = await authService.me(req.user.id);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const verifyEmail = async (req, res, next) => {
+    try {
+        await authService.verifyEmail(req.params.token);
+        res.json("Success")
+        // res.redirect(`${process.env.FRONTEND_URL}/signin`);
+    } catch (error) {
+        res.json(error)
+        // res.redirect(`${process.env.FRONTEND_URL}email/verified`);
+    }
+};
+
