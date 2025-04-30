067ba0b2b6ad17865d1280238459e087caca8d39
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 12:54:17 +0700

    Add : Package service

---
+++ src/services/package.service.js
@@ -0,0 +1,52 @@
+import * as packageRepository from "../repositories/package.repository.js";
+import { uploadImage } from "../utils/upload.utils.js";
+import { packageValidator } from "../utils/validators/index.js";
+import { NotFoundError, BadRequestError } from "../utils/errors.utils.js";
+
+export const getPackages = async () => {
+    const packages = await packageRepository.getPackages();
+    return packages;
+};
+
+export const getPackageById = async (id) => {
+    const getPackageById = await packageRepository.getPackageById(id);
+    if (!getPackageById) throw new NotFoundError("Package tidak ditemukan");
+    return getPackageById;
+};
+
+export const createPackage = async (data, file) => {
+    const { error } = packageValidator(data);
+    if (error) throw new BadRequestError("Validasi gagal", error.details.map(err => err.message));
+
+    const { title, price, benefit } = data;
+
+    const numericPrice = parseInt(price, 10);
+    const thumbnail = await uploadImage(file, "package");
+
+    const createPackage = await packageRepository.createPackage(data, numericPrice, thumbnail.fileUrl);
+    return createPackage;
+};
+
+export const updatePackage = async (id, data, file) => {
+    const getPackageById = await packageRepository.getPackageById(id);
+    if (!getPackageById) throw new NotFoundError("Package tidak ditemukan");
+
+    const { error } = packageValidator(data);
+    if (error) throw new BadRequestError("Validasi gagal", error.details.map(err => err.message));
+
+    const { title, price, benefit } = data;
+
+    const numericPrice = parseInt(price, 10);
+    const thumbnail = await uploadImage(file, "package");
+
+    const updatePackage = await packageRepository.updatePackage(id, data, numericPrice, thumbnail.fileUrl);
+    return updatePackage;
+};
+
+export const deletePackage = async (id) => {
+    const getPackageById = await packageRepository.getPackageById(id);
+    if (!getPackageById) throw new NotFoundError("Package tidak ditemukan");
+
+    await packageRepository.deletePackage(id);
+    return { message: "Package berhasil dihapus" };
+};
