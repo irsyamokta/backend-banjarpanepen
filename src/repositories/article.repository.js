5d1739d51da5bbecd4612da76ca5f9cbd7dfd55f
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 12:53:29 +0700

    Update : Article repository

--- src/repositories/article.repository.js
+++ src/repositories/article.repository.js
@@ -1,10 +1,20 @@
 import prisma from "../config/db.js";
 
-export const getArticles = async () => prisma.article.findMany();
+export const getArticles = async () => {
+    return await prisma.article.findMany({
+        include: {
+            editor: {
+                select: {
+                    name: true,
+                }
+            }
+        }
+    });
+};
 
 export const getArticleById = async (id) => prisma.article.findUnique({ where: { id } });
 
-export const createArticle = async (data, file) => {
+export const createArticle = async (userId, data, file) => {
     return prisma.article.create(
         {
             data:
@@ -12,12 +22,22 @@ export const createArticle = async (data, file) => {
                 title: data.title,
                 content: data.content,
                 writer: data.writer,
-                editorId: data.userId,
+                editorId: userId,
                 thumbnail: file
             }
         });
 };
 
-export const updateArticle = async (id, data, file) => prisma.article.update({ where: { id }, data, file });
+export const updateArticle = async (id, data, file) => {
+    return prisma.article.update({
+        where: { id },
+        data: {
+            title: data.title,
+            content: data.content,
+            writer: data.writer,
+            thumbnail: file
+        }
+    });
+};
 
 export const deleteArticle = async (id) => prisma.article.delete({ where: { id } });
