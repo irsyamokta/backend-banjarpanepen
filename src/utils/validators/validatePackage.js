8732393001c79527cd516af23cf1090f4915bba6
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 12:54:56 +0700

    Add : Validate package

---
+++ src/utils/validators/validatePackage.js
@@ -0,0 +1,32 @@
+import Joi from "joi";
+
+export const packageValidator = (data) => {
+    const schema = Joi.object({
+        title: Joi.string()
+            .min(3)
+            .max(100)
+            .required()
+            .messages({
+                "string.min": "Judul minimal harus 3 karakter!",
+                "string.max": "Judul maksimal 100 karakter!",
+                "any.required": "Judul tidak boleh kosong!"
+            }),
+
+        price: Joi.number()
+            .min(3)
+            .messages({
+                "number.min": "Harga minimal adalah 3!",
+                "any.required": "Harga tidak boleh kosong!"
+            }),
+
+        benefit: Joi.string()
+            .min(3)
+            .required()
+            .messages({
+                "string.min": "Benefit minimal harus 3 karakter!",
+                "any.required": "Benefit tidak boleh kosong!"
+            }),
+    });
+
+    return schema.validate(data, { abortEarly: false });
+};
