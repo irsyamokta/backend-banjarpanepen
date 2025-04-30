75929fc4bf2fc8f2d2b8a9e685cee8ea176d0209
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 19:16:46 +0700

    Add : Event routes

---
+++ src/routes/event.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getEvents, getEventById, createEvent, updateEvent, deleteEvent } from "../controllers/event.controller.js";
+import { multerUpload } from "../config/multer.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { hasRole } from "../middlewares/role.middleware.js";
+
+const router = express.Router();
+const isAdmin = hasRole("ADMIN");
+
+router.get("/all-event", getEvents);
+router.get("/:id", getEventById);
+router.post("/create-event", authMiddleware, isAdmin, multerUpload, createEvent);
+router.put("/update-event/:id", authMiddleware, isAdmin, multerUpload, updateEvent);
+router.delete("/delete-event/:id", authMiddleware, isAdmin, deleteEvent);
+
+export default router;
