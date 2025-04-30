6f9ad7379ffb67560d4e7474dd1c0d9b7c649bcc
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 12:54:40 +0700

    Update : Endpoint validators

--- src/utils/validators/index.js
+++ src/utils/validators/index.js
@@ -1,6 +1,7 @@
 import { registerValidator, loginValidator } from "./validateAuth.js";
 import { updateUserValidator } from "./validateUser.js";
 import { articleValidator } from "./validateArticle.js";
+import { packageValidator } from "./validatePackage.js";
 // import { createArticleValidator, updateArticleValidator } from "./validateArticle.js";
 // import { changePasswordValidator, resetPasswordValidator, forgotPasswordValidator } from "./validatePassword.js";
 // import { createNewsValidator, updateNewsValidator, newsStatusValidator } from "./validateNews.js";
@@ -10,6 +11,7 @@ export {
     loginValidator, 
     updateUserValidator,
     articleValidator,
+    packageValidator,
     // changePasswordValidator, 
     // resetPasswordValidator, 
     // forgotPasswordValidator,
