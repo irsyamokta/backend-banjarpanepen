1cf5c7e5d2d87c466cc64022d3c17e0fe9a3f26c
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-30 19:32:48 +0700

    Refactor : Article service

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -3,8 +3,8 @@ import { uploadImage } from "../utils/upload.utils.js";
 import { articleValidator } from "../utils/validators/index.js";
 import { NotFoundError, BadRequestError } from "../utils/errors.utils.js";
 
-export const getArticles = async () => {
-    const articles = await articleRepository.getArticles();
+export const getArticles = async (page, limit) => {
+    const articles = await articleRepository.getArticles(page, limit);
     return articles;
 };
 
@@ -21,7 +21,15 @@ export const createArticle = async (userId, data, file) => {
     const { title, content, writer } = data;
     const thumbnail = await uploadImage(file, "article");
 
-    const createArticle = await articleRepository.createArticle(userId, data, thumbnail.fileUrl);
+    const dataArticle = {
+        title,
+        content,
+        writer,
+        editorId: userId,
+        thumbnail: thumbnail.fileUrl
+    };
+
+    const createArticle = await articleRepository.createArticle(dataArticle);
     return createArticle;
 };
 
@@ -29,14 +37,21 @@ export const updateArticle = async (id, data, file) => {
 
     const article = await articleRepository.getArticleById(id);
     if (!article) throw new NotFoundError("Artikel tidak ditemukan");
-    
+
     const { error } = articleValidator(data);
     if (error) throw new BadRequestError("Validasi gagal", error.details.map(err => err.message));
 
     const { title, content, writer } = data;
     const thumbnail = await uploadImage(file, "article");
 
-    const updateArticle = await articleRepository.updateArticle(id, data, thumbnail.fileUrl);
+    const dataArticle = {
+        title,
+        content,
+        writer,
+        thumbnail: thumbnail.fileUrl
+    };
+
+    const updateArticle = await articleRepository.updateArticle(id, dataArticle);
     return updateArticle;
 };
 
