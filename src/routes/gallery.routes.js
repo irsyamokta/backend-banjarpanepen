bb11077e63cd25545cf456de9927640ecc36a7a1
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-02 22:57:32 +0700

    Update : Method route

--- src/routes/gallery.routes.js
+++ src/routes/gallery.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getGalleries, getGalleryById, createGallery, deleteGallery } from "../controllers/gallery.controller.js";
+import { getGalleries, getGalleryById, createGallery, deleteGallery, updateGallery } from "../controllers/gallery.controller.js";
 import { multerUpload } from "../config/multer.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { hasRole } from "../middlewares/role.middleware.js";
@@ -10,6 +10,7 @@ const isAdmin = hasRole("ADMIN");
 router.get("/all-gallery", getGalleries);
 router.get("/:id", getGalleryById);
 router.post("/create-gallery", authMiddleware, isAdmin, multerUpload, createGallery);
+router.patch("/update-gallery/:id", authMiddleware, isAdmin, multerUpload, updateGallery);
 router.delete("/delete-gallery/:id", authMiddleware, isAdmin, deleteGallery);
 
 export default router;
