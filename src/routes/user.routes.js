cafa620e1141b4b79a5107121a22dc0dc252f4ae
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-06 23:06:20 +0700

    Update : User route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { multerUpload } from "../config/multer.js";
-import { getUsers, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { getUsers, getUserByContact, updateUser, deleteUser } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { hasRole } from "../middlewares/role.middleware.js";
 
 const router = express.Router();
 const isAdmin = hasRole("ADMIN");
 
-router.get("/all-user", authMiddleware, isAdmin, getUsers)
+router.get("/contact", getUserByContact);
+router.get("/all-user", authMiddleware, isAdmin, getUsers);
 router.patch("/update", authMiddleware, isAdmin, multerUpload, updateUser);
 router.delete("/delete", authMiddleware, isAdmin, deleteUser);
 
